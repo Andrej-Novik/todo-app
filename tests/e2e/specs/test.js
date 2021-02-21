d934b9fc2386f093363493e00ff0293d97197638
Andrej-Novik <andrejnovikweb@gmail.com>
2021-02-21 18:17:38 +0300

    feature/P1-7: - changes for e2e tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -8,53 +8,58 @@ describe("e2e tests", () => {
   });
   it("test for input", () => {
     cy.get("#addTask").type("Task 1");
-    cy.wait(500);
     cy.get("#addTask").submit("prevent");
     cy.get("#addTask").type("Task 2");
-    cy.wait(500);
     cy.get("#addTask").submit("prevent");
     cy.get("#addTask").type("Task 3");
-    cy.wait(500);
     cy.get("#addTask").submit("prevent");
 
-    cy.get("#tasks")
-      .first()
-      .should("contain", "Task 1");
-    cy.wait(1000);
+    cy.get("#tasks").should("contain", "Task 1");
 
-    cy.get("#check")
+    cy.get("#tasks>label")
       .first()
       .click();
-    cy.wait(1000);
 
-    cy.get("#text").should("have.css", "color", "rgb(175, 175, 173)");
+    cy.get("#tasks>label #text")
+      .first()
+      .should("have.css", "color", "rgb(175, 175, 173)");
 
     cy.get("#tabs>label")
       .eq(1)
       .click();
-    cy.wait(1000);
+    cy.get("#tasks>label #text").should("have.css", "color", "rgb(0, 0, 0)");
+
     cy.get("#tabs>label")
       .eq(2)
       .click();
-    cy.wait(1000);
+    cy.get("#tasks>label #text").should(
+      "have.css",
+      "color",
+      "rgb(175, 175, 173)"
+    );
     cy.get("#tabs>label")
       .first()
       .click();
-    cy.wait(1500);
+    cy.get("#tasks>label #text").should(
+      "have.css",
+      "color",
+      "rgb(175, 175, 173)"
+    );
 
-    cy.get("#delete")
-      .first()
-      .invoke("show");
-    cy.wait(500);
-    cy.get("#delete").click();
-    cy.wait(500);
-
-    cy.get("#check")
-      .first()
+    cy.get("#tasks>label #delete")
+      .eq(0)
+      .invoke("show")
       .click();
-    cy.get("#text").should("have.css", "color", "rgb(175, 175, 173)");
-    cy.wait(1000);
+    cy.get("#tasks").should("have.length", 1);
+
+    cy.get("#tasks>label").click({ multiple: true });
+    cy.get("#tasks>label #text").should(
+      "have.css",
+      "color",
+      "rgb(175, 175, 173)"
+    );
 
     cy.get("#clear").click();
+    cy.get("#tasks").should("have.length", 0);
   });
 });
