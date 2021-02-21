4d00d1f0587c64d2084daaa68b72058953be0bc1
Andrej-Novik <andrejnovikweb@gmail.com>
2021-02-21 19:43:12 +0300

    feature/P1-7: - splitting tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,9 +3,11 @@ describe("e2e tests", () => {
   beforeEach(() => {
     cy.visit("http://localhost:8080");
   });
+
   it("loads the app", () => {
     cy.get("#page").should("be.visible");
   });
+
   it("test for input", () => {
     cy.get("#addTask")
       .type("Task 1")
@@ -20,9 +22,11 @@ describe("e2e tests", () => {
 
     cy.get("#tasks>label").should("contain", "Task 1", "Task 2", "Task 3");
     cy.get("#tasks>label").should("have.length", 3);
-
-    cy.get("#clear").click();
-    cy.get("#tasks>label").should("have.length", 3);
+  });
+  it("change status", () => {
+    cy.get("#addTask")
+      .type("Task 1")
+      .submit("prevent");
 
     cy.get("#tasks>label")
       .first()
@@ -31,6 +35,21 @@ describe("e2e tests", () => {
     cy.get("#tasks>label #checkbox")
       .first()
       .should("be.checked");
+  });
+  it("test for footer tabs", () => {
+    cy.get("#addTask")
+      .type("Task 1")
+      .submit("prevent");
+    cy.get("#addTask")
+      .type("Task 2")
+      .submit("prevent");
+    cy.get("#addTask")
+      .type("Task 3")
+      .submit("prevent");
+
+    cy.get("#tasks>label")
+      .first()
+      .click();
 
     cy.get("#tabs>label")
       .eq(1)
@@ -46,12 +65,31 @@ describe("e2e tests", () => {
       .first()
       .click();
     cy.get("#tasks>label #checkbox").should("have.length", 3);
+  });
+  it("delete one task", () => {
+    cy.get("#addTask")
+      .type("Task 1")
+      .submit("prevent");
 
     cy.get("#tasks>label #delete")
       .eq(0)
       .invoke("show")
       .click();
-    cy.get("#tasks").should("have.length", 1);
+    cy.get("#tasks>label").should("have.length", 0);
+  });
+  it("delete all task", () => {
+    cy.get("#addTask")
+      .type("Task 1")
+      .submit("prevent");
+    cy.get("#addTask")
+      .type("Task 2")
+      .submit("prevent");
+    cy.get("#addTask")
+      .type("Task 3")
+      .submit("prevent");
+
+    cy.get("#clear").click();
+    cy.get("#tasks>label").should("have.length", 3);
 
     cy.get("#tasks>label").click({ multiple: true });
     cy.get("#tasks>label #checkbox").should("be.checked");
