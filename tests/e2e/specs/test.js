b86cf8145e2f10ce125b04624b62280f59f9e5ef
Andrej-Novik <andrejnovikweb@gmail.com>
2021-02-21 19:11:52 +0300

    feature/P1-7: - add new e2e tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -7,44 +7,45 @@ describe("e2e tests", () => {
     cy.get("#page").should("be.visible");
   });
   it("test for input", () => {
-    cy.get("#addTask").type("Task 1");
-    cy.get("#addTask").submit("prevent");
-    cy.get("#addTask").type("Task 2");
-    cy.get("#addTask").submit("prevent");
-    cy.get("#addTask").type("Task 3");
+    cy.get("#addTask")
+      .type("Task 1")
+      .submit("prevent");
+    cy.get("#addTask")
+      .type("Task 2")
+      .submit("prevent");
+    cy.get("#addTask")
+      .type("Task 3")
+      .submit("prevent");
     cy.get("#addTask").submit("prevent");
 
-    cy.get("#tasks").should("contain", "Task 1");
+    cy.get("#tasks>label").should("contain", "Task 1", "Task 2", "Task 3");
+    cy.get("#tasks>label").should("have.length", 3);
+
+    cy.get("#clear").click();
+    cy.get("#tasks>label").should("have.length", 3);
 
     cy.get("#tasks>label")
       .first()
       .click();
 
-    cy.get("#tasks>label #text")
+    cy.get("#tasks>label #checkbox")
       .first()
-      .should("have.css", "color", "rgb(175, 175, 173)");
+      .should("be.checked");
 
     cy.get("#tabs>label")
       .eq(1)
       .click();
-    cy.get("#tasks>label #text").should("have.css", "color", "rgb(0, 0, 0)");
+    cy.get("#tasks>label #checkbox").should("not.be.visible");
 
     cy.get("#tabs>label")
       .eq(2)
       .click();
-    cy.get("#tasks>label #text").should(
-      "have.css",
-      "color",
-      "rgb(175, 175, 173)"
-    );
+    cy.get("#tasks>label #checkbox").should("be.checked");
+
     cy.get("#tabs>label")
       .first()
       .click();
-    cy.get("#tasks>label #text").should(
-      "have.css",
-      "color",
-      "rgb(175, 175, 173)"
-    );
+    cy.get("#tasks>label #checkbox").should("have.length", 3);
 
     cy.get("#tasks>label #delete")
       .eq(0)
@@ -53,11 +54,7 @@ describe("e2e tests", () => {
     cy.get("#tasks").should("have.length", 1);
 
     cy.get("#tasks>label").click({ multiple: true });
-    cy.get("#tasks>label #text").should(
-      "have.css",
-      "color",
-      "rgb(175, 175, 173)"
-    );
+    cy.get("#tasks>label #checkbox").should("be.checked");
 
     cy.get("#clear").click();
     cy.get("#tasks").should("have.length", 0);
